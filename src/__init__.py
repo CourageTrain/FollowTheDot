2d30edbb9604d795de1d214452fd954ab8e4c666
Vamsavardhana Vijay <vvamsava@gmail.com>
2026-02-13 17:15:03 +0530

    wip

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,1 @@
-__version__="0.1.0"
-
-from src.eyetrax_game.game import Game
-from src.eyetrax_game.patterns import Pattern, PatternType, create_pattern
-
-__all__ = [
-    "Game",
-    "Pattern",
-    "PatternType",
-    "create_pattern"
-]
 
